cb8a0e48c8bfe78887bce17a7053bb0760e600e5
3Pex <jh1036079@student.francistuttle.edu>
2024-12-21 13:19:35 -0600

    added stlyes to price compare, recent aactions, adn vboquet compare

--- obj/Debug/net8.0/Flower.AssemblyInfo.cs
+++ obj/Debug/net8.0/Flower.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Flower")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b949ef2027fe85397ccc48064c93e31e14cf5e0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5778412ad3fff1b27b84c8255aa9485e8123ff11")]
 [assembly: System.Reflection.AssemblyProductAttribute("Flower")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Flower")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
