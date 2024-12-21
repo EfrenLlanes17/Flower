073b6c3f5d2206a1b6068adaf0fdf88be98991ae
3Pex <jh1036079@student.francistuttle.edu>
2024-12-21 13:16:40 -0600

    added stlyes to the price compare tab

--- obj/Debug/net8.0/Flower.AssemblyInfo.cs
+++ obj/Debug/net8.0/Flower.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Flower")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4fc3d4d06f68570d0604740023b3529c99187058")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6822ee509ad1d49c7d3ddf4fded504e43867cffc")]
 [assembly: System.Reflection.AssemblyProductAttribute("Flower")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Flower")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
