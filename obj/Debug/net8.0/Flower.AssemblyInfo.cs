4fc3d4d06f68570d0604740023b3529c99187058
3Pex <jh1036079@student.francistuttle.edu>
2024-12-21 08:18:39 -0600

    changed the font size, color on the home page

--- obj/Debug/net8.0/Flower.AssemblyInfo.cs
+++ obj/Debug/net8.0/Flower.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Flower")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b1267eb2f2f26b78799bd8c5590804a33c7ecad")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e94bc723ddb8942345e2c5ea65e952386810d262")]
 [assembly: System.Reflection.AssemblyProductAttribute("Flower")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Flower")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
