a73001820ff3aeba6436d424f5459c47dd4a931a
3Pex <jh1036079@student.francistuttle.edu>
2024-12-24 11:55:54 -0600

    made new enrry look better

--- obj/Debug/net8.0/Flower.AssemblyInfo.cs
+++ obj/Debug/net8.0/Flower.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Flower")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7bc83d6c661798d2e25a1321bca375e71c18157a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99a519ad476ecfcff60747a4093a5c0fc3d3cc9c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Flower")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Flower")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
