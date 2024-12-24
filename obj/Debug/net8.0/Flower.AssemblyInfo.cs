5e2914fede1d01576e5556ec874f91a3a11d666c
EfrenLlanes17 <efrenllanes17@gmail.com>
2024-12-24 12:46:29 -0600

    100% Tested And Working Version

--- obj/Debug/net8.0/Flower.AssemblyInfo.cs
+++ obj/Debug/net8.0/Flower.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Flower")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c8f55d21da56f20ceb1e33669af324826c078f58")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f053f2b18dd45dad6b4d16ee0b5778ef1279d7e6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Flower")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Flower")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
