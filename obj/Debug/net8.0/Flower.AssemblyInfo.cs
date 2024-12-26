7a546bd02aec5369c2cf5beffa3984e3edd2ba3d
EfrenLlanes17 <efrenllanes17@gmail.com>
2024-12-25 23:32:55 -0600

    added Try Catch to dataset talking code to Add new entry, price compare, and bouquet recipes.

--- obj/Debug/net8.0/Flower.AssemblyInfo.cs
+++ obj/Debug/net8.0/Flower.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Flower")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1544c34a2ab44769624b59046ecbe6360de3e5d0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+481129ccc5a23efa3137f5e1c3ba7809b36233c7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Flower")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Flower")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
