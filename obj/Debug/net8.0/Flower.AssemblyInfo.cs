3481354ea818b59a017997c2f9033cbbefc0b9ec
3Pex <jh1036079@student.francistuttle.edu>
2024-12-26 18:36:42 -0600

    did the newline and save shit on new entry page

--- obj/Debug/net8.0/Flower.AssemblyInfo.cs
+++ obj/Debug/net8.0/Flower.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Flower")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+80aac927b96f0a0cfdfaee51b53f6f0d8b817cb4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8052530584913eac37b2bdc8466fa1daab836f12")]
 [assembly: System.Reflection.AssemblyProductAttribute("Flower")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Flower")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
