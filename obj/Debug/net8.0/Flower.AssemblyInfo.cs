1dbfe86d23e98cd83674920e262ad8c2a1087366
EfrenLlanes17 <efrenllanes17@gmail.com>
2024-12-29 05:12:10 -0600

    Added Try Catch to Undo Button in recent Acttions as well as made writetxt() work on any computer with file fetch

--- obj/Debug/net8.0/Flower.AssemblyInfo.cs
+++ obj/Debug/net8.0/Flower.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Flower")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+19309762e53d067494b4f0adc3bb0d1be8aec3d7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce761b2d3b6af6a1ece8ff82ff67ad853c55b337")]
 [assembly: System.Reflection.AssemblyProductAttribute("Flower")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Flower")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
