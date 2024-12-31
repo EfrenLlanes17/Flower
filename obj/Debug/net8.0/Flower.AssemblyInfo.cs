c691e5262fc593d2d1fc73bd3787c2f363d88837
EfrenLlanes17 <efrenllanes17@gmail.com>
2024-12-30 22:03:14 -0600

    Completed All checks on new entry before saving to database

--- obj/Debug/net8.0/Flower.AssemblyInfo.cs
+++ obj/Debug/net8.0/Flower.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Flower")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+23710dc5d12c5233c62fb0ecb25fbb595a51fa76")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+46ee6014dab3af5ca30b8bebcf18e17b5620522e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Flower")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Flower")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
