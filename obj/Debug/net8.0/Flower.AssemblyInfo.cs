5d31cdeb38818a2f4d7f11af76a22c4a319e5064
EfrenLlanes17 <efrenllanes17@gmail.com>
2025-01-01 03:00:27 -0600

    All Dropdowns are sorted alphabetically

--- obj/Debug/net8.0/Flower.AssemblyInfo.cs
+++ obj/Debug/net8.0/Flower.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Flower")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6dc297dbd170311c86b88182f1d2015071135d9a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+316d06ebf01565cb154b4505dcefc54e7b37e312")]
 [assembly: System.Reflection.AssemblyProductAttribute("Flower")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Flower")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
