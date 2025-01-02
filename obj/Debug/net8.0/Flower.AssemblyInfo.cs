0753ee1a5e12a2779ba87d88c126dd7948109715
EfrenLlanes17 <efrenllanes17@gmail.com>
2025-01-02 06:00:00 -0600

    Made Recent actions display from top to bottom instead of bottom to top

--- obj/Debug/net8.0/Flower.AssemblyInfo.cs
+++ obj/Debug/net8.0/Flower.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Flower")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4dbb306be95e61e175e48a661f119e7d95ac54c2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ed12bfde75c8afbca2abc1d50ecc20085983241")]
 [assembly: System.Reflection.AssemblyProductAttribute("Flower")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Flower")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
