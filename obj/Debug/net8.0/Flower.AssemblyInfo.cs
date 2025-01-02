6e86e15fb7d49f8bc0bd062beb2fd7df5de6752d
3Pex <jh1036079@student.francistuttle.edu>
2025-01-01 22:36:04 -0600

    fixed the stuff on add new entry

--- obj/Debug/net8.0/Flower.AssemblyInfo.cs
+++ obj/Debug/net8.0/Flower.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Flower")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4aba07744aba5fd2ffe6671ca4b691944cc68305")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2aee1faf304ecbcb56d99de2cb7b7deab56c8a23")]
 [assembly: System.Reflection.AssemblyProductAttribute("Flower")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Flower")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
