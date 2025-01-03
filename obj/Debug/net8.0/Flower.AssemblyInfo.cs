b9d8901f73948ec8ed0222a690df952329f0f458
EfrenLlanes17 <efrenllanes17@gmail.com>
2025-01-03 01:41:28 -0600

    Added Invalid type and Flower Name in New Entry

--- obj/Debug/net8.0/Flower.AssemblyInfo.cs
+++ obj/Debug/net8.0/Flower.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Flower")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4537e5ae67f94f601835038e0b68b79e1941f326")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c6ae0e8c51999b1e72f2971f111557de59e606e5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Flower")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Flower")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
