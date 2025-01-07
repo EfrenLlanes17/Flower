b5653d0680521060c753d6643dfff8beca089367
EfrenLlanes17 <efrenllanes17@gmail.com>
2025-01-06 23:05:42 -0600

    Version 1.0

--- obj/Debug/net8.0/Flower.AssemblyInfo.cs
+++ obj/Debug/net8.0/Flower.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Flower")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+235cd704ab8c0952a363b16d5f0c14c5b6ebefb5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c251bf79bc28141083ccaf522fd41b3ba7fb7664")]
 [assembly: System.Reflection.AssemblyProductAttribute("Flower")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Flower")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
