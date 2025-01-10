cca5e333188fb02a68ca18fa3d6e0905efa7732a
3Pex <jh1036079@student.francistuttle.edu>
2025-01-10 13:31:35 -0600

    did the css for quick view

--- obj/Debug/net8.0/Flower.AssemblyInfo.cs
+++ obj/Debug/net8.0/Flower.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Flower")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b8bacb8dba2b427c7b79ccfacb40ea3eeec1ff3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d845cea5a3dd9e884be6bbd61630e9cd01cb3d48")]
 [assembly: System.Reflection.AssemblyProductAttribute("Flower")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Flower")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
