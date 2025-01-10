dd31bc9a297af493aac2b9ffb36332afe1f9fa5e
EfrenLlanes17 <efrenllanes17@gmail.com>
2025-01-10 11:10:24 -0600

    Recent Actions Does not let user remove only purchase from flower

--- obj/Debug/net8.0/Flower.AssemblyInfo.cs
+++ obj/Debug/net8.0/Flower.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Flower")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7342992f21200dc09848e8b56ab97816be6c966e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b8bacb8dba2b427c7b79ccfacb40ea3eeec1ff3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Flower")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Flower")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
