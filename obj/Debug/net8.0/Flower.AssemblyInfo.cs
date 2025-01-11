bee05674073f87973fc934799771b93fa556087f
EfrenLlanes17 <efrenllanes17@gmail.com>
2025-01-11 06:56:36 -0600

    Bouquet Quick View sorts by Retail Price

--- obj/Debug/net8.0/Flower.AssemblyInfo.cs
+++ obj/Debug/net8.0/Flower.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Flower")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+84ee8e0fb4d1713166814d341100714faf81f297")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c9e7b28ae375f36b15d4afd54f0d153e28984b4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Flower")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Flower")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
